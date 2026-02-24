1d437aae8e85f76c18d87a4fb96823722327b532
Ingrid Silva <ingridsilvatec@outlook.com>
2026-02-24 19:09:36 -0300

    add video sample

---
+++ app/src/main/java/br/com/jpegsinng/videoplayer/MainActivity.kt
@@ -0,0 +1,31 @@
+package br.com.jpegsinng.videoplayer
+
+import android.net.Uri
+import android.net.Uri.parse
+import android.os.Bundle
+import android.widget.MediaController
+import androidx.appcompat.app.AppCompatActivity
+import br.com.jpegsinng.videoplayer.databinding.ActivityMainBinding
+import androidx.core.net.toUri
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val videoView = binding.videoView
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+
+        val uri: Uri = ("android.resource://" + packageName
+                + "/raw/videosample").toUri()
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.start()
+    }
+}
